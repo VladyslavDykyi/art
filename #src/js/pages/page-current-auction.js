ffa10e10046fd530c36d9e7170f3f90cd556d64b
VladyslavDykyi <vladislavdykyi33@gmail.com>
2023-06-26 13:10:19 +0300

    upd page-current-auction

--- #src/js/pages/page-current-auction.js
+++ #src/js/pages/page-current-auction.js
@@ -3,23 +3,20 @@
 ( function (){
 	const swiper = new Swiper(".mySwiper", {
 		slidesPerView: 1,
-		spaceBetween: 10,
-		pagination: {
-			el: ".swiper-pagination",
-			clickable: true,
+		centeredSlidesBounds: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
 		},
 		breakpoints: {
 			640: {
 				slidesPerView: 2,
-				spaceBetween: 20,
 			},
 			768: {
-				slidesPerView: 4,
-				spaceBetween: 40,
+				slidesPerView: 3,
 			},
 			1024: {
-				slidesPerView: 5,
-				spaceBetween: 50,
+				slidesPerView: 4,
 			},
 		},
 	});
