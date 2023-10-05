8816fb02e192ff2bd9d2549493a6f58aa57204ff
VladyslavDykyi <vladislavdykyi33@gmail.com>
2023-10-05 21:25:40 +0300

    upd 30.09.2023

--- #src/js/pages/page-live-auction.js
+++ #src/js/pages/page-live-auction.js
@@ -15,4 +15,32 @@
 	messageList.scrollTo(0, messageList.scrollHeight);
 
 
+
+
+	// window.addEventListener('DOMContentLoaded', ()=> {
+	// 	const messageItem = document.querySelector('.auction-list .current-lot');
+	// 	messageItem.scrollIntoView({behavior: 'auto', block:'start'});
+	// })
+	// const auctionList = document.querySelector('.auction-list');
+	//
+	// const currentLot = document.querySelector('.auction-lot.current-lot');
+	// auctionList.addEventListener('scroll',()=> {
+	// 	console.dir(currentLot)
+	// })
+
+	const elementToScrollTo = document.querySelector(".auction-list > .current-lot");
+
+
+	const windowHeight = window.innerHeight;
+	const pageHeight = document.body.scrollHeight;
+
+	const elementRect = elementToScrollTo.getBoundingClientRect();
+	const elementTop = elementRect.top;
+	const elementHeight = elementRect.height;
+	const scrollToY = elementTop - (windowHeight / 2 - elementHeight / 2);
+
+	window.scrollTo({
+		top: scrollToY,
+		behavior: "auto",
+	});
 })();
